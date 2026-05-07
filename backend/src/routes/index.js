49050b0e3a455adbf39f19a2f5756a0ef7259dda
Kabil <kabilk2023@gmail.com>
2026-05-07 12:57:32 +0530

    Delete backend directory

--- backend/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.use("/auth", require("./authRoutes"));
-router.use("/attendance", require("./attendanceRoutes"));
-router.use("/admin", require("./adminRoutes"));
-
-module.exports = router;
